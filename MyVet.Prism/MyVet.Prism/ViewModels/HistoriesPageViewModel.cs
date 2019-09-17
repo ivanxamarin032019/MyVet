dede010039cfa4db34d660e69cdd3ed7955cec22
Ivan Xamarin <ivanxamarin032019@hotmail.com>
2019-09-18 00:47:04 +0200

    Part 43 - History Page

--- MyVet.Prism/MyVet.Prism/ViewModels/HistoriesPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/HistoriesPageViewModel.cs
@@ -1,20 +1,23 @@
 ﻿using MyVet.Common.Models;
 using Prism.Navigation;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace MyVet.Prism.ViewModels
 {
     public class HistoriesPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
         private PetResponse _pet;
-        private ObservableCollection<HistoryResponse> _histories;
+        private ObservableCollection<HistoryItemViewModel> _histories;
 
         public HistoriesPageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            _navigationService = navigationService;
             Title = "Histories";
         }
 
-        public ObservableCollection<HistoryResponse> Histories
+        public ObservableCollection<HistoryItemViewModel> Histories
         {
             get => _histories;
             set => SetProperty(ref _histories, value);
@@ -34,7 +37,14 @@ namespace MyVet.Prism.ViewModels
             {
                 Pet = parameters.GetValue<PetResponse>("pet");
                 Title = $"Histories of {Pet.Name}";
-                Histories = new ObservableCollection<HistoryResponse>(Pet.Histories);
+                Histories = new ObservableCollection<HistoryItemViewModel>(Pet.Histories.Select(h => new HistoryItemViewModel(_navigationService)
+                {
+                    Date = h.Date,
+                    Description = h.Description,
+                    Id = h.Id,
+                    Remarks = h.Remarks,
+                    ServiceType = h.ServiceType
+                }).ToList());
             }
         }
     }
