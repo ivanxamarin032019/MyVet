755ad70273a650ee4bc98d98aad91e0deaf003f8
Ivan Xamarin <ivanxamarin032019@hotmail.com>
2019-09-23 15:55:56 +0200

    Part 52 - Recover Password

--- MyVet.Web/Helpers/IUserHelper.cs
+++ MyVet.Web/Helpers/IUserHelper.cs
@@ -26,6 +26,19 @@ namespace MyVet.Web.Helpers
         Task<IdentityResult> UpdateUserAsync(User user);
 
         Task<SignInResult> ValidatePasswordAsync(User user, string password);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        Task<User> GetUserByIdAsync(string userId);
+
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
     }
 }
 
